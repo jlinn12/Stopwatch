3d81a6bc94486f89aeb7f86d19dad6d0acd9b38d
Juan Linneberg <juanlinneberg@Juans-MacBook-Pro.local>
2022-06-17 08:47:30 +0200

    Minutes and Seconds added

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+const clockSeconds = document.getElementById('clock-seconds');
+const clockMinutes = document.getElementById('clock-minutes');
 const clock = document.getElementById('clock');
 const start = document.getElementById('start');
 const stop = document.getElementById('stop');
@@ -6,12 +8,24 @@ const reset = document.getElementById('reset');
 
 let myVar = setInterval(myTimer, 1000);
 
-let secs = 0;
 
+let secs = 0;
+let minutes = 0;
+const emptyVal = 0;
+           
 function myTimer() {
-    
  secs++;
 
- clock.innerHTML = secs;
+ if (secs == 60) { minutes++; 
+    clockMinutes.innerHTML = minutes;
+    secs = 0
+    clockSeconds.innerHTML = "0" + secs;
+ } 
+ if (secs <= 9) {
+    clockSeconds.innerHTML = "0" + secs;
+ }
+ 
+ if (secs > 9) {
+    clockSeconds.innerHTML = secs;
 
-}
+ }}
